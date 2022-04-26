41355d88b8dbfa1cb0ab19bc37223831add36f55
Jeremy Fields <j.c.delafields@gmail.com>
2022-04-26 20:49:55 +0000

    Added base 721 logic

--- contracts/HotPotato.sol
+++ contracts/HotPotato.sol
@@ -1,26 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+// https://medium.com/gft-engineering/creating-your-own-nft-from-scratch-and-listing-it-on-opensea-8ac296cf1019
+// https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-remix/6358
+// https://wizard.openzeppelin.com/#erc721
 
-contract HotPotato is ERC721, Ownable {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/**
+ * @title Hot Potato
+ * @dev ERC721 with novel hot potato logic
+ */
+contract HotPotato is ERC721, ERC721URIStorage, Pausable, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+
+    Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Hot Potato", "SPUD") {}
 
-    function mintNFT(address _receiver, string memory _tokenURI) 
-        external  
-        returns (uint256) 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override
     {
-        _tokenIds.increment();
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
-        uint 256 newNftTokenId = _tokenIds.current();
-        _safeMint(receiver, newNftTokenId);
-        _setTokenURI(newNftTokenId);
+    // The following functions are overrides required by Solidity.
 
-        return newNftTokenId;
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
